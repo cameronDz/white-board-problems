2d535b3c20e49f726be43f511ec7b5478e37da8d
cameronDz <camerondziurgot@gmail.com>
2018-11-20 17:37:05 -0500

    Added constructor test for App.

--- src/test/java/org/md/education/AppTest.java
+++ src/test/java/org/md/education/AppTest.java
@@ -1,11 +1,21 @@
 package org.md.education;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
-	public void main_emptyConstructor_run() {
+	public void main_static_run() {
 		App.main("");
+	}
+
+	@Test
+	public void constructor_empty_run() {
+		App app = new App();
+		String expected = "App";
+		String actual = app.getClass().getSimpleName();
+		assertEquals(expected, actual);
 	}
 }
