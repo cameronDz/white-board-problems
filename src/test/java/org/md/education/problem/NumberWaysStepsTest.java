e150bca335b4648babad61612f118def8bf8ee82
cameronDz <camerondziurgot@gmail.com>
2018-11-20 16:47:33 -0500

    Added blank tests.

---
+++ src/test/java/org/md/education/problem/NumberWaysStepsTest.java
@@ -0,0 +1,11 @@
+package org.md.education.problem;
+
+import org.junit.Test;
+
+public class NumberWaysStepsTest {
+
+	@Test
+	public void test() {
+		// TODO write tests
+	}
+}
---
+++ src/test/java/org/md/education/problem/ValidateNumberWaysStepsTest.java
@@ -0,0 +1,11 @@
+package org.md.education.problem;
+
+import org.junit.Test;
+
+public class ValidateNumberWaysStepsTest {
+
+	@Test
+	public void test() {
+		// TODO write tests
+	}
+}
